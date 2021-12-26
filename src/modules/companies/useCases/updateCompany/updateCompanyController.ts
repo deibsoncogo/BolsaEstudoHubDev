695c9d6a2722947f262ddb9cf32c1321662d0865
Deibson Cogo <deibsoncogo@outlook.com>
2021-12-26 16:29:57 -0300

    Ajuste na validação de dados e regra de negócio da alteração de empresa, updateCompany

--- src/modules/companies/useCases/updateCompany/updateCompanyController.ts
+++ src/modules/companies/useCases/updateCompany/updateCompanyController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import * as yup from "yup";
+import { AppError } from "../../../../errors/appError";
+import { YupSetLocale } from "../../../../utils/yupSetLocale";
 import { UpdateCompanyService } from "./updateCompanyService";
 
 export class UpdateCompanyController {
@@ -9,6 +12,32 @@ export class UpdateCompanyController {
       corporateName, fantasyName, cnpj, departamento, contact, email,
       userOwnerId, addressId,
     } = request.query;
+
+    if (!corporateName && !fantasyName && !cnpj && !departamento && !contact && !email
+      && !userOwnerId && !addressId) {
+      throw new AppError("Não foi informado nenhum valor para alteração", 200);
+    }
+
+    try {
+      YupSetLocale();
+
+      const schemaParams = yup.object().shape({ id: yup.string().required().uuid() });
+
+      const schemaQuery = yup.object().shape({
+        corporateName: yup.string(),
+        fantasyName: yup.string(),
+        cnpj: yup.number().integer().positive(),
+        departamento: yup.string(),
+        contact: yup.number().integer().positive(),
+        userOwnerId: yup.string().uuid(),
+        addressId: yup.string().uuid(),
+      });
+
+      await schemaParams.validate(request.params, { abortEarly: false });
+      await schemaQuery.validate(request.query, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error.errors, 401);
+    }
 
     const updateCompanyService = container.resolve(UpdateCompanyService);
 
--- src/modules/companies/useCases/updateCompany/updateCompanyService.ts
+++ src/modules/companies/useCases/updateCompany/updateCompanyService.ts
@@ -1,11 +1,23 @@
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/appError";
+import { IUserRepository } from "../../../users/repositories/iUserRepository";
 import { IUpdateCompanyDto } from "../../dtos/iUpdateCompanyDto";
 import { CompanyEntity } from "../../entities/companyEntity";
+import { IAddressRepository } from "../../repositories/iAddressRepository";
 import { ICompanyRepository } from "../../repositories/iCompanyRepository";
 
 @injectable()
 export class UpdateCompanyService {
-  constructor(@inject("CompanyRepository") private companyRepository: ICompanyRepository) { }
+  constructor(
+    @inject("CompanyRepository")
+    private companyRepository: ICompanyRepository,
+
+    @inject("UserRepository")
+    private userRepository: IUserRepository,
+
+    @inject("AddressRepository")
+    private addressRepository: IAddressRepository,
+  ) { }
 
   async execute(
     {
@@ -13,6 +25,37 @@ export class UpdateCompanyService {
       userOwnerId, addressId,
     }: IUpdateCompanyDto,
   ): Promise<CompanyEntity> {
+    const idCompanyExists = await this.companyRepository.findIdCompany(id);
+
+    if (!idCompanyExists) {
+      throw new AppError("Não existe este ID de empresa cadastrado");
+    }
+
+    const corporateNameAlreadyExists = await this.companyRepository.findOneCorporateName(
+      corporateName,
+    );
+
+    if (corporateNameAlreadyExists) {
+      throw new AppError("Já existe esta ração social cadastrada");
+    }
+
+    const cnpjAlreadyExists = await this.companyRepository.findOneCnpj(cnpj);
+
+    if (cnpjAlreadyExists) {
+      throw new AppError("Já existe este CNPJ cadastrado");
+    }
+
+    const userOwnerIdExists = await this.userRepository.findOneIdUser(userOwnerId);
+
+    if (userOwnerId && !userOwnerIdExists) {
+      throw new AppError("Não existe um usuário com este ID");
+    }
+
+    const addressIdExists = await this.addressRepository.findOneIdAddress(addressId);
+
+    if (addressId && !addressIdExists) {
+      throw new AppError("Não existe um endereço com este ID");
+    }
     const companyUpdate = await this.companyRepository.updateCompany({
       id,
       corporateName,
