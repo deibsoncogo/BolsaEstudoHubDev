902a30303e180094a567786ad2e63e000ae2c111
Deibson Cogo <deibsoncogo@outlook.com>
2021-12-26 15:39:53 -0300

    Ajuste na validação de dados da busca com filtro de endereço, findFilterAddress

--- src/modules/companies/useCases/findFilterAddress/findFilterAddressController.ts
+++ src/modules/companies/useCases/findFilterAddress/findFilterAddressController.ts
@@ -1,10 +1,30 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import * as yup from "yup";
+import { AppError } from "../../../../errors/appError";
+import { YupSetLocale } from "../../../../utils/yupSetLocale";
 import { FindFilterAddressService } from "./findFilterAddressService";
 
 export class FindFilterAddressController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id, publicPlace, number, state, city, country } = request.query;
+
+    try {
+      YupSetLocale();
+
+      const schema = yup.object().shape({
+        id: yup.string().uuid(),
+        publicPlace: yup.string(),
+        number: yup.string(),
+        state: yup.string(),
+        city: yup.string(),
+        country: yup.string(),
+      });
+
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error.errors, 401);
+    }
 
     const findFilterAddressService = container.resolve(FindFilterAddressService);
 
