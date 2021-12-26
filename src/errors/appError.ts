13b11d1f028390810bfbef48fd96d98694535d7c
Deibson Cogo <deibsoncogo@outlook.com>
2021-12-26 17:26:04 -0300

    Correção de erro no appError

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,10 +1,13 @@
+/* eslint-disable no-debugger */
 export class AppError { // classe que vai criar uma tratativa especial para os erros
   public readonly message: string; // cria uma variável publica somente leitura
 
   public readonly statusCode: number; // cria uma variável publica somente leitura
 
   constructor(message: string, statusCode = 400) { // criar a mensagem de erro
-    this.message = message[0] || message; // define a mensagem
+    const isStringMessage = typeof message === "string";
+
+    this.message = isStringMessage ? message : message[0]; // define a mensagem
     this.statusCode = statusCode; // define o código do erro
   }
 }
