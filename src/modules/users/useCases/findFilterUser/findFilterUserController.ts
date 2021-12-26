3321bc4c2f8d3b0b314ec025cabfe8a0ad2b3c47
Deibson Cogo <deibsoncogo@outlook.com>
2021-12-26 15:14:29 -0300

    Ajuste na validação de dados da busca com filtro de usuário, findFilterUser

--- src/modules/users/useCases/findFilterUser/findFilterUserController.ts
+++ src/modules/users/useCases/findFilterUser/findFilterUserController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import * as yup from "yup";
+import { AppError } from "../../../../errors/appError";
+import { YupSetLocale } from "../../../../utils/yupSetLocale";
 import { FindFilterUserService } from "./findFilterUserService";
 
 export class FindFilterUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id, name, cpf, email } = request.query;
+
+    try {
+      YupSetLocale();
+
+      const schemaQuery = yup.object().shape({
+        id: yup.string().uuid(),
+        name: yup.string(),
+        cpf: yup.string(),
+        email: yup.string().email(),
+      });
+
+      await schemaQuery.validate(request.query, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error.errors, 401);
+    }
 
     const findFilterUserService = container.resolve(FindFilterUserService);
 
