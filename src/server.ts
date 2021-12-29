dec143fb4498360b0ab05dc5610a35358077291c
Deibson Cogo <deibsoncogo@outlook.com>
2021-12-28 21:53:33 -0300

    Criação dos arquivos iniciais da documentação

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import "./database";
 import "./containers";
 import express from "express";
 import "express-async-errors";
+import swagger from "swagger-ui-express";
+import swaggerJson from "../docs/swagger.json";
 import { ErrorMiddleware } from "./middlewares/errorMiddleware";
 import { indexRouter } from "./routes";
 
 const app = express();
 
+app.use("/doc", swagger.serve, swagger.setup(swaggerJson));
 app.use(express.json());
 app.use(indexRouter);
 app.use(ErrorMiddleware);
